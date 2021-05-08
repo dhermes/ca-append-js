77d720b7c1780bfa14beaef1d29017dc3c3cba4a
Danny Hermes <dhermes@blend.com>
2021-05-07 22:29:00 -0500

    Adding "new" context options to extended context options.

--- index.ts
+++ index.ts
@@ -16,7 +16,35 @@ import * as tls from 'tls'
 
 export const wrappedTLSCreateSecureContext = tls.createSecureContext
 
-export interface SecureContextOptions extends tls.SecureContextOptions {}
+export interface SecureContextOptions extends tls.SecureContextOptions {
+  /**
+   * List of trusted CA certificates to append to the default chain. If `caReplace` is **also**
+   * provided, this field will append to whatever list has replaced the default chain.
+   *
+   * See: https://nodejs.org/docs/latest-v12.x/api/tls.html#tls_tls_createsecurecontext_options
+   */
+  caAppend?: string | Buffer | Array<string | Buffer>
+  /**
+   * List of trusted CA certificates to replace the default chain. This behaves as the original `ca` would behave
+   *
+   * See: https://nodejs.org/docs/latest-v12.x/api/tls.html#tls_tls_createsecurecontext_options
+   */
+  caReplace?: string | Buffer | Array<string | Buffer>
+  /**
+   * This is "necessary" to re-apply the `NODE_EXTRA_CA_CERTS` environment variable in cases where it's ignored.
+   * There is a Node.js bug that causes `NODE_EXTRA_CA_CERTS` to be ignored if `ctx.context.addCACert()` has been
+   * called on the `SecureContext` returned from `tls.createSecureContext`. So when `caAppend` has been set, we
+   * invoke `ctx.context.addCACert()` and invalidate `NODE_EXTRA_CA_CERTS`.
+   *
+   * This will append `NODE_EXTRA_CA_CERTS` to the default chain if:
+   * - `appendNodeExtraCACerts` is `true`
+   * - `caAppend` has at least one value
+   * - `NODE_EXTRA_CA_CERTS` is set (if this points to an invalid filesystem path, it was cause an error to be thrown)
+   *
+   * See: https://github.com/nodejs/node/issues/32010
+   */
+  appendNodeExtraCACerts?: boolean
+}
 
 function modifiedCreateSecureContext(details: SecureContextOptions): tls.SecureContext {
   const ctx = wrappedTLSCreateSecureContext(details)
