771263b84194de061c05628ab61b1de510e1efab
Danny Hermes <dhermes@blend.com>
2021-05-07 22:49:47 -0500

    Running `prettier` on `env.ts`.

--- env.ts
+++ env.ts
@@ -12,16 +12,16 @@
  * limitations under the License.
  */
 
-import * as fs from 'fs';
-import * as process from 'process';
+import * as fs from 'fs'
+import * as process from 'process'
 
 export function nodeExtraCACerts(): Buffer | undefined {
-  const extraCACertsPath = process.env.NODE_EXTRA_CA_CERTS;
+  const extraCACertsPath = process.env.NODE_EXTRA_CA_CERTS
   if (!extraCACertsPath) {
-    return undefined;
+    return undefined
   }
 
   // NOTE: This will throw an exception if `extraCACertsPath` does not exist. This is by design; it makes it very clear
   //       to the user if `NODE_EXTRA_CA_CERTS` is not set correctly.
-  return fs.readFileSync(extraCACertsPath);
+  return fs.readFileSync(extraCACertsPath)
 }
